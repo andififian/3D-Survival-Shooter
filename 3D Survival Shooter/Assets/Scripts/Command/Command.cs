ebd93d614acf09c3293e20742df7219d9227aacc
andififian <70189494+andififian@users.noreply.github.com>
2021-09-29 21:36:58 +0700

    1st commit

---
+++ 3D Survival Shooter/Assets/Scripts/Command/Command.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Command
+{
+    public abstract void Execute();
+    public abstract void UnExecute();
+    // // Start is called before the first frame update
+    // void Start()
+    // {
+        
+    // }
+
+    // // Update is called once per frame
+    // void Update()
+    // {
+        
+    // }
+}
